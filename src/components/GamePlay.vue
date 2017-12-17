c494cc14267b5bec6bfb4a2b46f7b23828a7eafb
Quang <trannhatquang3112@gmail.com>
2017-12-18 03:05:20 +0800

    add task and propose list

--- src/components/GamePlay.vue
+++ src/components/GamePlay.vue
@@ -4,18 +4,49 @@
   <h1>This is GamePlay</h1>
   <h2 v-if="isHappening">Let's get down to business!</h2>
   <h2 v-else>Waiting for other players...</h2>
+
   <div v-if="inLobby">
   <input type="text" placeholder = "What is your cat name?" v-model="playerID"></input>
   <button v-on:click="createNewPlayer()">Go!</button>
   </div>
+  
+  <img v-if="inLobby" src="https://www.iizcat.com/uploads/2017/04/kr490-bc4.jpg" alt = "Title">
+
 
   <div class = "vertical-menu" v-else>
     <a v-for="player in playerList">
       <img src="https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/0b/0bc3ded6d1c690449dd74dee852f6053517749cb_full.jpg" alt = "Ava">
       <li>Player: {{ player.username }}</li>
       <li>Score: {{ player.highscore }}</li>
-      <li>Height: {{ player.strength }}</li>
+      <li>Stamina: {{ player.strength }}</li>
     </a>
+  </div>
+
+  <div class = "vertical-profit" v-if="inLobby === false">
+     <a v-for="profit in profitList" v-on:click="startPropose()">
+        <img src="http://chefspalate.com.au/wp-content/uploads/2016/05/6.png" alt = "Ava">
+        <li>Task {{ profit.Order }}</li>
+        <li>Profit: {{ profit.value }}</li>
+        <li>Stamina: {{ profit.cost }}</li>
+    </a>
+  </div>
+  <div v-if="isProposing">
+     <input type="text" placeholder = "Which task?" v-model="task_order"></input>
+     <input type="text" placeholder = "Player 1: Share" v-model="propose1"></input>
+     <input type="text" placeholder = "Player 2: Share" v-model="propose2"></input>
+     <input type="text" placeholder = "Player 3: Share" v-model="propose3"></input>
+     <button v-on:click="propose()">Propose!</button>
+  </div>
+
+  <div class = "scroll" v-if="inLobby==false">
+     <ul v-for="propose in proposeList" v-on:click="startPropose()">
+        <li>Task {{ propose.number }}</li>
+        <li>Share1: {{ propose.share1 }}</li>
+        <li>Share2: {{ propose.share2 }}</li>
+        <li>Share3: {{ propose.share3 }}</li>
+        <button>Yes</button>
+        <button>No</button>
+    </ul>
   </div>
 
 
@@ -23,7 +54,9 @@
 
 
 
-  <router-link to = "/gameend">End the game?</router-link>
+
+
+  <router-link to = "/gameend" v-if="inLobby==false">End the game?</router-link>
   </div>
 </template>
 
@@ -58,23 +91,59 @@ userRef.on('value', (snapshot) => {
     console.log(Object.values(snapshot.val()));
 });
 
+var proposeRef = database.ref().child('proposes');
+proposeRef.on('value', (snapshot) => {
+    this.proposeList = Object.values(snapshot.val());
+    console.log(Object.values(snapshot.val()));
+});
+
 export default {
   name: 'GameStart',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
       playerList: [],
+      profitList: [],
+      proposeList: [],
       isHappening: true,
       playerID:'',
       inLobby: true,
       height: 0,
+      isProposing: false,
+      task_order: '',
+      propose1: '',
+      propose2: '',
+      propose3: '',
 
     }
   },
   methods: {
+    propose: function() {
+        firebase.database().ref('proposes/' + this.playerID).set({
+           number: this.task_order,
+           share1: this.propose1,
+           share2: this.propose2,
+           share3: this.propose3,
+        });
+        this.isProposing = false
+        this.task_order =  ''
+        this.propose1 = ''
+        this.propose2 = ''
+        this.propose3 = ''
+    },
+
+
      createNewPlayer: function() {
          writeUserData(this.playerID, 'true', '0', this.height);
          this.inLobby = false;
+     },
+
+     byPass: function() {
+         this.inLobby = false;
+     },
+
+     startPropose: function() {
+         this.isProposing = !this.isProposing;
      },
 
 
@@ -86,7 +155,15 @@ export default {
      database.ref().child('users').on('value', (snapshot) => {
      this.playerList = Object.values(snapshot.val());
      this.height = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
-  });
+     });
+
+     database.ref().child('profits').on('value', (snapshot) => {
+     this.profitList = Object.values(snapshot.val());
+     });
+
+     database.ref().child('proposes').on('value', (snapshot) => {
+     this.proposeList = Object.values(snapshot.val());
+     });
 
 
   },
@@ -97,6 +174,7 @@ export default {
 <style scoped>
 .vertical-menu {
     width: 250px; /* Set a width if you like */
+    padding: 12px;
 }
 
 .vertical-menu a {
@@ -116,10 +194,52 @@ export default {
     color: white;
 }
 
-img {
+.vertical-menu a img {
     width: 50px; /* you can use % */
     height: auto;
+    float: left;
 }
+
+.vertical-profit {
+    width: 500px; /* Set a width if you like */
+    padding: 12px;
+}
+
+.vertical-profit a {
+    background-color: #eee; /* Grey background color */
+    color: black; /* Black text color */
+    display: block; /* Make the links appear below each other */
+    padding: 12px; /* Add some padding */
+    text-decoration: none; /* Remove underline from links */
+}
+
+.vertical-profit a:hover {
+    background-color: #ccc; /* Dark grey background on mouse-over */
+}
+
+.vertical-profit a:hover img {
+    transform: scale(1.5);
+}
+
+
+.vertical-profit a.active {
+    background-color: #4CAF50; /* Add a green color to the "active/current" link */
+    color: white;
+}
+
+.vertical-profit a img {
+    width: 100px; /* you can use % */
+    height: auto;
+    float: left;
+}
+
+.scroll {
+    width: 600px;
+    height: 100px;
+    overflow: scroll;
+}
+
+
 
 h1, h2 {
   font-weight: normal;
