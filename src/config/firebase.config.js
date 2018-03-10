a8ccdffd006c3f8043e0861478dd602e6c81d768
nguyentaduy <nguyentaduy@hotmail.com>
2018-03-10 17:07:56 +0800

    update gitignore

--- src/config/firebase.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: "AIzaSyBEHPA-ti-TC5m5L5aZD4ZwwOZaNtZo-lk",
-  authDomain: "cat-app-a1391.firebaseapp.com",
-  databaseURL: "https://cat-app-a1391.firebaseio.com",
-  projectId: "cat-app-a1391",
-  storageBucket: "",
-  messagingSenderId: "1067089159725"
-};
